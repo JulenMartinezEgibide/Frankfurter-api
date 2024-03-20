680753cefb3cfe95b77d93ec2991836f5e525a37
Julen <julen.martinez@ikasle.egibide.org>
2024-03-20 18:10:37 +0100

    Frankfuter 1.0

---
+++ FrankfurterAPI/FrankfurterAPI/Views/DivisaDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DivisaDetailView.swift
+//  FrankfurterAPI
+//
+//  Created by  on 20/3/24.
+//
+
+import SwiftUI
+
+struct DivisaDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisaDetailView()
+}
---
+++ FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
@@ -0,0 +1,18 @@
+//
+//  DivisaListView.swift
+//  FrankfurterAPI
+//
+//  Created by  on 20/3/24.
+//
+
+import SwiftUI
+
+struct DivisaListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisaListView()
+}
