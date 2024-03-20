22abaf2ba12197e8f2a69758ebd8c46c0fdacc06
Julen <julen.martinez@ikasle.egibide.org>
2024-03-20 18:28:25 +0100

    .

--- FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
+++ FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct DivisaListView: View {
+    var divisa: Divisa
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Amount: \(divisa.amount, specifier: "%.2f")")
+                Text("Base: \(divisa.base)")
+                Text("Date: \(divisa.date)")
+                Text("Rates:")
+                
+                List {
+                    ForEach(divisa.rates.sorted(by: { $0.key < $1.key }), id: \.key) { key, value in
+                        NavigationLink(destination: DivisaDetailView(divisaName: key, divisaValue: value)) {
+                            HStack {
+                                Text("\(key):")
+                                Spacer()
+                                Text("\(value, specifier: "%.4f")")
+                            }
+                        }
+                    }
+                    .listStyle(PlainListStyle()) // Optional: to remove default list style
+                }
+            }
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
+            .padding()
+            .navigationTitle("Divisa List")
+        }
     }
 }
 
 #Preview {
-    DivisaListView()
+    DivisaListView(divisa: Mockdata.divisa)
 }
