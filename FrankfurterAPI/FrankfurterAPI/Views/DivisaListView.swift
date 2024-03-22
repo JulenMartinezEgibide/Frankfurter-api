ae67a3d3ca46a43cbf1201863da3dec4fc0bb6bc
Julen <julen.martinez@ikasle.egibide.org>
2024-03-22 17:44:24 +0100

    remake

--- FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
+++ FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
@@ -11,13 +11,6 @@ struct DivisaListView: View {
     var divisa: Divisa
     
     var body: some View {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< 680753cefb3cfe95b77d93ec2991836f5e525a37
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-=======
-=======
->>>>>>> 424f2a85a0fc296f37c381b0c4467a89810e69dc
         NavigationView {
             VStack(alignment: .leading, spacing: 10) {
                 Text("Amount: \(divisa.amount, specifier: "%.2f")")
@@ -27,14 +20,14 @@ struct DivisaListView: View {
                 
                 List {
                     ForEach(divisa.rates.sorted(by: { $0.key < $1.key }), id: \.key) { key, value in
-                        NavigationLink(destination: DivisaDetailView(divisaName: key,divisaValue: value)) {
+                        NavigationLink(destination: DivisaDetailView(divisaName: key, divisaValue: value)) {
                             HStack {
                                 Text("\(key):")
                                 Spacer()
                                 Text("\(value, specifier: "%.4f")")
                             }
                         }
-                    } 
+                    }
                     .listStyle(PlainListStyle()) // Optional: to remove default list style
                 }
             }
@@ -44,15 +37,9 @@ struct DivisaListView: View {
             .padding()
             .navigationTitle("Divisa List")
         }
-<<<<<<< HEAD
->>>>>>> 1.1
-=======
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
->>>>>>> parent of 22abaf2 (.)
-=======
->>>>>>> 424f2a85a0fc296f37c381b0c4467a89810e69dc
     }
 }
+
 
 #Preview {
     DivisaListView(divisa: Mockdata.divisa)
