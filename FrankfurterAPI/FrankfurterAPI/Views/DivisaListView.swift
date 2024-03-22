7fb529ce1cd938013939f9a53e5b5e84b2240603
Julen <julen.martinez@ikasle.egibide.org>
2024-03-22 18:27:15 +0100

    list apear

--- FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
+++ FrankfurterAPI/FrankfurterAPI/Views/DivisaListView.swift
@@ -2,45 +2,33 @@
 //  DivisaListView.swift
 //  FrankfurterAPI
 //
-//  Created by  on 20/3/24.
+//  Created by  on 22/3/24.
 //
 
 import SwiftUI
 
-struct DivisaListView: View {
-    var divisa: Divisa
-    
+struct DivisaDetail: View {
+    let divisa: Divisa
+
     var body: some View {
-        NavigationView {
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Amount: \(divisa.amount, specifier: "%.2f")")
-                Text("Base: \(divisa.base)")
-                Text("Date: \(divisa.date)")
-                Text("Rates:")
-                
-                List {
-                    ForEach(divisa.rates.sorted(by: { $0.key < $1.key }), id: \.key) { key, value in
-                        NavigationLink(destination: DivisaDetailView(divisaName: key, divisaValue: value)) {
-                            HStack {
-                                Text("\(key):")
-                                Spacer()
-                                Text("\(value, specifier: "%.4f")")
-                            }
-                        }
-                    }
-                    .listStyle(PlainListStyle()) // Optional: to remove default list style
+        VStack(alignment: .leading) {
+            Text("Amount: \(divisa.amount)")
+            Text("Base: \(divisa.base)")
+            Text("Date: \(divisa.date)")
+            Text("Rates:")
+            
+            ForEach(divisa.rates.sorted(by: <), id: \.key) { key, value in
+                NavigationLink(destination: Text("\(key): \(value)")) {
+                    Text("\(key): \(value)")
                 }
             }
-            .padding()
-            .background(Color.gray.opacity(0.1))
-            .cornerRadius(10)
-            .padding()
-            .navigationTitle("Divisa List")
         }
+        .padding()
+        .navigationTitle("Divisa Detail")
     }
 }
 
-
 #Preview {
-    DivisaListView(divisa: Mockdata.divisa)
+    DivisaDetail(divisa: Mockdata.divisa)
 }
+
